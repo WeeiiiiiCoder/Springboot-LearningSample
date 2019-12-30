e46199ddf42cc0ffd87152587f2e1ea70eedf9f6
zhouwei <chouwe052@163.com>
2019-12-30 17:20:33 +0800

    修改Chapter3 fastjson版本

---
+++ Springboot-LearningSample/Chapter3-1-6/src/main/java/com/lazyboy/C316Application.java
@@ -0,0 +1,14 @@
+package com.lazyboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * XML
+ */
+@SpringBootApplication
+public class C316Application {
+    public static void main(String[] args) {
+        SpringApplication.run(C316Application.class, args);
+    }
+}
